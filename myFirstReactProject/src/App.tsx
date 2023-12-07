f8c84a4666ceb7fec54bd50b8acb4c031e9fdbc3
martleru <68188509+martleru@users.noreply.github.com>
2023-12-07 15:40:23 +0200

    Finished?

--- myFirstReactProject/src/App.tsx
+++ myFirstReactProject/src/App.tsx
@@ -1,33 +1,63 @@
-import { useState, FormEvent } from "react";
+import  { useState, FormEvent} from "react"; //useEffect
 import "./App.css";
+
+
+type Task = {
+  text: string;
+  completed: boolean;
+}
 
 const ToDoList = () => {
 
   const [input, setInput] = useState<string>("");
-  const [tasks, setTasks] = useState<{ text: string; completed: boolean }[]>([]);
+  const [tasks, setTasks] = useState<Task []>([]);
+  
+
+
+  console.log(tasks)
+  
+  // useEffect(() => {
+  //   localStorage.setItem('myTask', JSON.stringify(tasks));
+  // }, [tasks]); 
+
+  // useEffect(() => {
+  //   const storedTasks = localStorage.getItem('myTask');
+  //   if (storedTasks) {
+  //     setTasks(JSON.parse(storedTasks));
+  //   }
+  // }, []); 
+
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     if (input.trim() !== "") {
-      setTasks((prevTasks) => [
-        ...prevTasks,
-        { text: input, completed: false },
-      ]);
+      const newTask: Task = {
+        text: input,
+        completed: false,
+      };
+
+      const newTasks = [...tasks, newTask];
+      setTasks(newTasks);
       setInput("");
     }
   };
 
   const handleCheckboxChange = (index: number) => {
-    setTasks((prevTasks) =>
-      prevTasks.map((task, i) =>
-        i === index ? { ...task, completed: !task.completed } : task
-      )
-    );
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
   };
 
+  const handleDelete = (index: number) => {
+    const allTasks = [...tasks];
+    allTasks.splice(index, 1)
+    setTasks(allTasks)
+  }
+
   return (
-    <div>
+    <div className="container">
+      <h1>To-Do List</h1>
       <form className="todo-form" onSubmit={handleSubmit}>
         <input
           type="text"
@@ -41,21 +71,34 @@ const ToDoList = () => {
         </button>
       </form>
 
-      <ul>
+      <div className="task-table">
         {tasks.map((task, index) => (
-          <li>
+          <div 
+            key={index}
+            className="single-task"
+          >
             <input
               type="checkbox"
               checked={task.completed}
-              onChange={() => handleCheckboxChange(index)}
+              onChange={() =>handleCheckboxChange(index)}
             />
-            <span style={{textDecoration: task.completed ? "line-through" : "none",}}>
+            <span
+              style={{textDecoration: task.completed ? "line-through" : "none",
+              fontSize: '24px'}}
+            >
               {task.text}
             </span>
-          </li>
+            <span>
+              <button
+                className="delete-btn"
+                onClick={() => handleDelete(index)}
+              >
+                  Delete
+              </button>
+            </span>
+          </div>
         ))}
-      </ul>
-
+      </div>
     </div>
   );
 };
